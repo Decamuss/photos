e8b8870227a9fef73814d993b8d6bc393be9ff99
Alex W <alex.seungmin.woo@gmail.com>
2023-11-21 00:48:12 -0500

    java doc

--- Photos68/src/Photos.java
+++ Photos68/src/Photos.java
@@ -9,10 +9,19 @@ import utils.DataManager;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * The main application class for the Photo Application.
+ * Extends the JavaFX Application class to create a JavaFX application.
+ */
 public class Photos extends Application {
 
     private static List<User> users;
 
+    /**
+     * The entry point of the application.
+     *  main passes into this method to start the app visualization
+     * @param args The command line arguments passed to the application.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Load users
@@ -30,14 +39,30 @@ public class Photos extends Application {
         });
     }
 
+    /**
+     * Starts the JavaFX application.
+     *
+     * @param args is the primaryStage, The primary stage for this application, onto which the application scene can be set.
+     * @throws IOException If an error occurs during the loading of the FXML file.
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Gets the list of users currently loaded in the application.
+     *
+     * @return The list of users.
+     */
     public static List<User> getUsers() {
         return users;
     }
 
+    /**
+     * Sets the list of users for the application.
+     *
+     * @param users The list of users to be set.
+     */
     public static void setUsers(List<User> users) {
         Photos.users = users;
     }
--- Photos68/src/utils/DataManager.java
+++ Photos68/src/utils/DataManager.java
@@ -9,16 +9,32 @@ import java.util.stream.Collectors;
 
 import model.User;
 
+/**
+ * Utility class for managing user data, providing methods to save, load, add, and delete user information.
+ */
 public class DataManager {
 
     private static final String FILE_PATH = "data/users.ser"; // Updated file path
-
+    /**
+     * Saves a list of users to the specified file path.
+     *
+     * @param users The list of users to be saved.
+     * @throws IOException If an I/O error occurs during the serialization process.
+     */
     public static void saveUsers(List<User> users) throws IOException {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
             out.writeObject(users);
         }
     }
 
+    /**
+     * Loads a list of users from the specified file path.
+     * If the file does not exist, creates a new file and returns an empty list.
+     *
+     * @return The list of users loaded from the file or an empty list if the file is empty or not found.
+     * @throws IOException            If an I/O error occurs during the deserialization process.
+     * @throws ClassNotFoundException If the class of a serialized object cannot be found.
+     */
     public static List<User> loadUsers() throws IOException {
         File file = new File(FILE_PATH);
         if (!file.exists()) {
@@ -39,7 +55,11 @@ public class DataManager {
         }
     }
     
-
+    /**
+     * Adds a user to the list of users and saves the updated list.
+     *
+     * @param user The user to be added.
+     */
     public static void addUser(User user) {
         try {
             List<User> users = loadUsers();
@@ -51,7 +71,13 @@ public class DataManager {
         }
     }
     
-
+    /**
+     * Deletes a user with the specified username from the list of users and saves the updated list.
+     *
+     * @param username The username of the user to be deleted.
+     * @throws IOException            If an I/O error occurs during the serialization process.
+     * @throws ClassNotFoundException If the class of a serialized object cannot be found.
+     */
     public static void deleteUser(String username) throws IOException, ClassNotFoundException {
         List<User> users = loadUsers();
         users = users.stream().filter(u -> !u.getUsername().equals(username)).collect(Collectors.toList());
